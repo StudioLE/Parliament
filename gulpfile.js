04406dcf5b794626df971b9f9d40b2966e166f92
Laurence Elsdon <git@elsdon.me>
2016-10-03 14:21:53 +0100

    Gulp build method

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+'use strict'
+
+// Core modules
+var p = require('path')
+var ver = require('./package.json').version
+
+// Node modules
+var gulp = require('gulp')
+var gp_bump = require('gulp-bump')
+var gp_clean = require('gulp-clean')
+var gp_html = require('gulp-html-replace')
+var gp_concat = require('gulp-concat')
+var gp_rename = require('gulp-rename')
+var gp_uglify = require('gulp-uglify')
+var gp_clean_css = require('gulp-clean-css')
+var gp_less = require('gulp-less')
+var p = require('path')
+
+var build = {
+  /**
+   * Format build directory path
+   */
+  path: function(path) {
+    if( ! path) path = ''
+    return p.join('build', path)
+  },
+
+  /**
+   * Format version query string
+   */
+  version: function() {
+    return '?v' + ver
+  }
+}
+
+// Bump version
+gulp.task('bump', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(gp_bump({
+    // type:'prerelease'
+  }))
+  .pipe(gulp.dest('./'))
+})
+
+// Clean build directory
+ gulp.task('clean', function () {
+  return gulp.src('build', {
+    // read: false
+  })
+  .pipe(gp_clean())
+})
+
+// Build index.html
+gulp.task('index', function() {
+  gulp.src('src/index.html')
+  .pipe(gp_html({
+    'css': 'css/app.css' + build.version(),
+    'vendor-css': 'css/vendor.css' + build.version(),
+    'js': 'js/app.js' + build.version(),
+    'vendor-js': 'js/vendor.js' + build.version()
+  }))
+  .pipe(gulp.dest(build.path()))
+})
+
+// Copy static assets
+gulp.task('assets', function() {
+  // Images
+  gulp.src('src/img/*')
+  .pipe(gulp.dest(build.path('img')))
+  // Overpass Font
+  gulp.src('src/bower_components/overpass/Webfonts/*')
+  .pipe(gulp.dest(build.path('css')))
+  // Font Awesome
+  gulp.src('src/bower_components/font-awesome/fonts/*')
+  .pipe(gulp.dest(build.path('fonts')))
+  // Humans.txt
+  // gulp.src('src/humans.txt')
+  // .pipe(gulp.dest(build.path()))
+  // Favicon
+  // gulp.src('src/favicon.ico')
+  // .pipe(gulp.dest(build.path()))
+  // gulp.src('src/img/favicon.png')
+  // .pipe(gulp.dest(build.path('img')))
+})
+
+// Build app CSS
+gulp.task('css', function() {
+  gulp.src('src/css/style.less')
+  .pipe(gp_less({ paths: [
+    'src/bower_components/bootstrap/less/mixins.less',
+    'src/bower_components/bootstrap/less/variables.less'
+  ]}))
+  .pipe(gp_rename('app.css'))
+  .pipe(gp_clean_css({keepSpecialComments: 0}))
+  .pipe(gulp.dest(build.path('css')))
+})
+
+// Build vendor CSS
+gulp.task('vendor-css', function() {
+  gulp.src([
+    'src/bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'src/bower_components/overpass/Webfonts/recommended.css'
+  ])
+  .pipe(gp_concat('concat.js'))
+  .pipe(gp_rename('vendor.css'))
+  .pipe(gp_clean_css({keepSpecialComments: 0}))
+  .pipe(gulp.dest(build.path('css')))
+})
+
+// Build app JS
+gulp.task('js', function() {
+  gulp.src([
+      'src/js/app.js'
+    ])
+  .pipe(gp_concat('concat.js'))
+  .pipe(gp_rename('app.js'))
+  // .pipe(gp_uglify())
+  .pipe(gulp.dest(build.path('js')))
+})
+
+// Build vendor JS
+gulp.task('vendor-js', function() {
+  gulp.src([
+    'src/bower_components/jquery/dist/jquery.min.js',
+    'src/bower_components/bootstrap/dist/js/bootstrap.min.js',
+    'src/bower_components/lodash/dist/lodash.min.js',
+    'src/bower_components/d3/d3.min.js'
+  ])
+  .pipe(gp_concat('concat.js'))
+  .pipe(gp_rename('vendor.js'))
+  // .pipe(gp_uglify())
+  .pipe(gulp.dest(build.path('js')))
+})
+
+// Build task
+gulp.task('build', ['index', 'assets', 'css', 'vendor-css', 'js', 'vendor-js'])
+
+// Default task
+gulp.task('default', ['build'])
